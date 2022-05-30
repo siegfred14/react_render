b31b2620881bf043712c8f799c866dc7e5fd2820
Siegfred Samson <siegfredsamson@gmail.com>
2022-05-31 00:08:42 +0100

    React Render Notes

---
+++ src/components/Notes.js
@@ -0,0 +1,7 @@
+// useState v useReducer
+// Scenario            -          useState            -        useReducer
+// Type of State           Number, String, Boolean         Object or Array
+// No of state transitions        one or two                    too many
+// Related state transitions        No                            Yes
+// Business Logic             No business logic           Complex Business Logic
+// Local v Global state           local                           global
