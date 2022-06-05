59d30b73588c7159f48694f4a145d61bd6f85818
Siegfred Samson <siegfredsamson@gmail.com>
2022-06-05 10:16:02 +0100

    react render with useReducer, using counters

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import { UseReducer } from "./components/UseReducer/UseReducer";
 import { UseState } from "./components/UseState/UseState";
 
 function App() {
   return (
     <div className="App">
-      <UseState />
+      {/* <UseState /> */}
+      <UseReducer />
     </div>
   );
 }
