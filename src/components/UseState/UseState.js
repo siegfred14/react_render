bcea3502ea2d67d77987db7963659c7d45917811
Siegfred Samson <siegfredsamson@gmail.com>
2022-06-05 09:59:59 +0100

    re-rendering the UseState

--- src/components/UseState/UseState.js
+++ src/components/UseState/UseState.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 
 export const UseState = () => {
   const [count, setCount] = UseState(0);
@@ -6,13 +6,9 @@ export const UseState = () => {
   console.log("UseState Render");
   return (
     <div>
-      <button
-        onClick={() => {
-          setCount((c) => c + 1);
-        }}
-      >
-        count - {count}
-      </button>
+      <button onClick={() => setCount((c) => c + 1)}> count - {count} </button>
+      <button onClick={() => setCount(0)}>Count to 0</button>
+      <button onClick={() => setCount(5)}>Count to 5</button>
     </div>
   );
 };
