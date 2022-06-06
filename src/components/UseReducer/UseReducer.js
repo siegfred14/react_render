c082a4aeed7986e4ad8e7f78e6132eccf2dd6b5c
Siegfred Samson <siegfredsamson@gmail.com>
2022-06-06 23:43:10 +0100

    notes on useReducer and Render

--- src/components/UseReducer/UseReducer.js
+++ src/components/UseReducer/UseReducer.js
@@ -28,3 +28,9 @@ export const UseReducer = () => {
     </div>
   );
 };
+
+// useReducer          and            Render
+// The dispatch function from a useReducer hook will cause the component to re-render
+// The exception us when you update the state to the same value as he current state
+// Same value after the initial render? The component will not re-render
+// Same value after re-render? React will render tat specific component one more time and then bails out from from any subsequent renders.
