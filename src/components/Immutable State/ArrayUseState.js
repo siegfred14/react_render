36edf8209cb746b9e3325c5f739ce751977a9317
Siegfred Samson <siegfredsamson@gmail.com>
2022-06-10 08:21:41 +0100

    Notes on state Immutability and render

--- src/components/Immutable State/ArrayUseState.js
+++ src/components/Immutable State/ArrayUseState.js
@@ -29,3 +29,13 @@ export const ArrayUseState = () => {
     </div>
   );
 };
+
+// State Immutability and render
+// 1.) Mutating an Object or an array as state will not cause
+// a re-render when used with useState or useReducer hook
+
+// 2.) To re-render, make a copy of the existing state, modify as 
+// necessary and then pass the new state to the setter function
+
+// 3.) Directly mutating the state is an easy way to create bugs in your application.
+// Be sure to avoid that
