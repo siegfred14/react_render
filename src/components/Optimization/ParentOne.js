947638001d11e4db69ac2b4f3453d3b1198a3103
Siegfred Samson <siegfredsamson@gmail.com>
2022-06-13 08:09:28 +0100

    notes on same elements reference re-render

--- src/components/Optimization/ParentOne.js
+++ src/components/Optimization/ParentOne.js
@@ -1,5 +1,6 @@
 import { render } from "@testing-library/react";
-import React, { useState } from "react";
+import React, { Component, useState } from "react";
+import { ChildOne } from "./ChildOne";
 // import { ChildOne } from "./ChildOne";
 
 export const ParentOne = ({ children }) => {
@@ -20,3 +21,11 @@ export const ParentOne = ({ children }) => {
 // 2.) A component can render if it's parent component is rendered
 
 // The {children} component could be any name
+// component can change it's state but not it's props
+// React automatically provides the optimization
+// React looks at the OptParentOne Component
+// It converts button and children this.props.
+// Re-render is caused by a state chaange in OptParentOne
+// Since component has no means of directly changing the props, children props couldn't have changed
+// Instead of going through the render state for the ChildOne Component, we simply make use of the React element that was previously created
+// Children props has to be referencing the same element from the previous render, and will skip the render phase for the ChildOne component
