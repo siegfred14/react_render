13ffb6de4ee5c41f3ba8f45332179981483d0681
Siegfred Samson <siegfredsamson@gmail.com>
2022-06-14 08:41:19 +0100

    More notes on Same Element Reference

--- src/components/Optimization/GrandParent.js
+++ src/components/Optimization/GrandParent.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { unstable_renderSubtreeIntoContainer } from "react-dom";
 import { ChildOne } from "./ChildOne";
 import { ParentOne } from "./ParentOne";
 
@@ -15,3 +16,9 @@ export const GrandParent = () => {
     </div>
   );
 };
+
+// Same Element Reference
+// In React, when a parent component renders, React will recursively render all of it's child components.
+// This can however lead to "unnecesary renders" where the child component goes through the render phase but not the commit phase
+// So, IF YOU'RE EXPERIENCING PERFORMANCE ISSUES, you can extract the expensive child component and instead, pas it down as props to the parent component
+// In doing so, whenever there is a re-render caused by a change in the state of the parent component, React will optimize the re-render for you by knowing that the props has to be referencing the same element before and after the render
