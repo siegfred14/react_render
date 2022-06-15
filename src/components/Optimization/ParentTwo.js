898488d294517c84f8d37d38dfe88c3693d2bef2
Siegfred Samson <siegfredsamson@gmail.com>
2022-06-15 08:31:25 +0100

    Same element reference v React Memo

--- src/components/Optimization/ParentTwo.js
+++ src/components/Optimization/ParentTwo.js
@@ -16,3 +16,16 @@ export const ParentTwo = () => {
     </div>
   );
 };
+
+// WHEN TO USE SAME ELEMENT REFERENCE TECHNIQUE AND WHEN TO USE REACT.MEMO
+// Same Element Reference                               -   React Memo
+// 1.) When your parent component re-renders                When your child component is being asked to re-render
+// because of state change in the parent component          due to changes in the parent's state which do not affect
+// ---                                                      the child component prop in any way
+
+// 2.) This technique does not work if the parent
+// component re-renders because of hanges in
+// it's props
+
+// State change? Yes
+// Props change? No
