af3f8bda9c41bf735fef59b0a02d7e29816f4e49
Siegfred Samson <siegfredsamson@gmail.com>
2022-06-15 08:36:12 +0100

    initial incorrect optimization

--- src/components/Optimization/ChildTwo.js
+++ src/components/Optimization/ChildTwo.js
@@ -11,5 +11,10 @@ export const ChildTwo = () => {
 
 export const MemoizedChildTwo = React.memo(ChildTwo);
 
+// React.memo
+// In React, when a parent component renders, a child component might unnecesarily render
+// To optimize this behaviour, we can use React.memo and pass in the child component
+// It will re-render the child component only if the props have changed
+
 // NOTE: memo only does a shallow comparison of the previous and new props
 // However, we can pass in the custom comparison function as the second argument to React.memo to meet expected requirement
