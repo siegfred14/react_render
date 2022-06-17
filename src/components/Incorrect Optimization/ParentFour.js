fd78a2521eaf9f6388a22c2f8ada2a9ef152e504
Siegfred Samson <siegfredsamson@gmail.com>
2022-06-17 08:44:52 +0100

    optimization objects and functions as props using useMemo

--- src/components/Incorrect Optimization/ParentFour.js
+++ src/components/Incorrect Optimization/ParentFour.js
@@ -1,5 +1,5 @@
 import { render } from "@testing-library/react";
-import React, { Component, useState } from "react";
+import React, { Component, useState, useMemo } from "react";
 import { MemoizedChildFive } from "./ChildFive";
 
 export const ParentFour = () => {
@@ -11,12 +11,19 @@ export const ParentFour = () => {
     lname: "Siegfred",
   };
 
+  const memoizedPerson = useMemo(() => person, []);
+
+  const handleClick = () => {};
+
   console.log("ParentFour Render");
   return (
     <div>
       <button onClick={() => setCount((c) => c + 1)}> count - {count} </button>
       <button onClick={() => setName("Samson")}> Change Name</button>
-      <MemoizedChildFive name={name} person={person} />
+      <MemoizedChildFive name={name} person={memoizedPerson} />
     </div>
   );
 };
+
+// NB: If your component has objects or functions as props and you're wrapping one of it's child
+// components with React.memo, it is an incorrect usage of Memoization
